019e77dddd513a3a10b36a1db642e16e60355177
Moe Myat Zaw <mmzliveid@gmail.com>
2018-04-22 16:14:16 +0630

    v6.0.0-beta.0

--- samples/ng-logging-aspnetcore-sample/ClientApp/app/reducers/index.ts
+++ samples/ng-logging-aspnetcore-sample/ClientApp/app/reducers/index.ts
@@ -1,6 +1,5 @@
 import { storeLogger } from '@bizappframework/ngrx-store-logger';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-import { storeFreeze } from 'ngrx-store-freeze';
 
 import { environment } from 'environments/environment';
 
@@ -10,4 +9,4 @@ export interface State { }
 export const reducers: ActionReducerMap<State> = {};
 
 export const metaReducers: MetaReducer<State>[] = environment.production
-    ? [] : [storeLogger, storeFreeze];
+    ? [] : [storeLogger];
